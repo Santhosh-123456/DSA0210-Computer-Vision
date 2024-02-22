4413a6b2b18dd05bbb921499e6a6f04a43fc973d
Santhosh-123456 <124123012+Santhosh-123456@users.noreply.github.com>
2024-02-22 18:32:39 +0530

    Create 11.Rotation along 270 Degree.py

---
+++ 11.Rotation along 270 Degree.py
@@ -0,0 +1,7 @@
+import cv2
+path =r"C:\Users\palas\OneDrive\Pictures\Screenshots\Screenshot 2024-02-21 090822.png"
+src = cv2.imread(path)
+window_name = 'Image'
+image = cv2.rotate(src, cv2.ROTATE_90_COUNTERCLOCKWISE)
+cv2.imshow(window_name, image)
+cv2.waitKey(0)
